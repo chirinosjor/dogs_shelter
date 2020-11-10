6f3a97d6e77ef7bf60edf4b4fd8b68a31355ed56
Jorge Chirinos <chirinosjor@gmail.com>
2020-11-10 15:26:09 -0300

    Alliances section added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
     def index
         @shelters = Shelter.all
-        @dogs = Dog.all 
+        @dogs = Dog.all
+        @staffs = Staff.all  
         render :layout => false
     end
 end
